1bc9d4bdd62026a9ad11737ef47517f512185645
Rita Bradley <contact@ritabradley.com>
2019-03-10 22:12:16 -0700

    setup express server skeleton

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.listen(port, console.log(`Now listening on port: ${port}...`));
