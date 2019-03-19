18c2f2a5ad9258fe99ccf05aa4d9b0cac9d9df3c
Rita Bradley <contact@ritabradley.com>
2019-03-19 15:47:09 -0700

    move route handling to separate files

---
+++ controllers/profile.js
@@ -0,0 +1,20 @@
+const handleProfile = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*')
+    .from('users')
+    .where({
+      id,
+    })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json('not found');
+      }
+    })
+    .catch(err => res.status(404).json('error retrieving user'));
+};
+
+module.exports = {
+  handleProfile,
+};
